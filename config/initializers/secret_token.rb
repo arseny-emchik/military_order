9eeae139212a9ec5007ece5074edf51b940d16f5
ars <arsemchik@gmail.com>
2014-04-01 22:11:52 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MilitaryOrder::Application.config.secret_key_base = '37651914f49a025d00b39e13f7520cea1109c05a517e6757464ee1fada41931ddbb8c472ef2ce7a08a71cb327f95594c0b82bfb0153380f43630ddfcacf81c87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MilitaryOrder::Application.config.session_store :cookie_store, key: '_military_order_session'
