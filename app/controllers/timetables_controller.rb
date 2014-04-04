e92e4101bb4e54c399ddfe110412e324e9eb973b
ars <arsemchik@gmail.com>
2014-04-04 13:47:54 +0300

    reorganization main view structural + add schedule in view

--- app/controllers/timetables_controller.rb
+++ app/controllers/timetables_controller.rb
@@ -1,6 +1,9 @@
 class TimetablesController < ApplicationController
   def index
-
+    @color_day_off = '#faf0e6'
+    @current_month = Date.current.month
+    @current_year = Date.current.year
+    @soldiers = Soldier.all
   end
 
   private
