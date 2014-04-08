d2968aae22e98968ac868812309287e924e01874
ars <arsemchik@gmail.com>
2014-04-08 22:15:31 +0300

    data changes

--- app/controllers/timetables_controller.rb
+++ app/controllers/timetables_controller.rb
@@ -1,7 +1,7 @@
 class TimetablesController < ApplicationController
   def index
     @color_day_off = '#faf0e6'
-    @current_month = Date.current.month
+    @current_month = params[:month].to_i || Date.current.month
     @current_year = Date.current.year
     @soldiers = Soldier.all
   end
