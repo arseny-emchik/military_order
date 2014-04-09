270e8bd8e75e6333f4db940d16108b5b65f49308
ars <arsemchik@gmail.com>
2014-04-09 09:24:46 +0300

    data refactor

--- app/controllers/timetables_controller.rb
+++ app/controllers/timetables_controller.rb
@@ -1,12 +1,18 @@
 class TimetablesController < ApplicationController
   def index
     @color_day_off = '#faf0e6'
-    @current_month = params[:month].to_i || Date.current.month
-    @current_year = Date.current.year
+    @current_date = set_date
+
     @soldiers = Soldier.all
   end
 
   private
+
+  def set_date
+    current_month = params[:month] || Date.current.month
+    current_year = params[:year] || Date.current.year
+    Date.new(current_year.to_i, current_month.to_i, 1)
+  end
 
   def initialize_for_layout
     @current_tab = :timetables
