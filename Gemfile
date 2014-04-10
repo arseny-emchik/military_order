c4184a99c8c185df2173877369d9e3917d324c1d
ars <arsemchik@gmail.com>
2014-04-10 19:40:10 +0300

    added devise sign_in. a few rout problems

--- Gemfile
+++ Gemfile
@@ -48,4 +48,5 @@ gem 'simple_form'
 gem 'russian', '~> 0.6.0'
 gem 'momentjs-rails', '~> 2.5.0'
 gem 'bootstrap3-datetimepicker-rails', '~> 3.0.0'
+gem 'devise'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   Russian.init_i18n
 
+  #before_action :authenticate_user!
   layout :layout_by_resource
   before_filter :initialize_for_layout, except: [:new]
 
@@ -12,7 +11,21 @@ class ApplicationController < ActionController::Base
 
   def initialize_for_layout
   end
+
   def layout_by_resource
-    'main_view' #unless devise_controller?
+    if devise_controller?
+      'device'
+    else
+      'main_view'
+    end
   end
+
+  def after_sign_in_path_for(resource_or_scope)
+    timetables_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
 end
