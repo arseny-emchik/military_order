e188fc587c2e4a73cf10a3cb2273c1fbc6a658a5
ars <arsemchik@gmail.com>
2014-04-13 13:27:45 +0300

    corrected the content_tag(th) in the timetable_helper

--- app/helpers/timetables_helper.rb
+++ app/helpers/timetables_helper.rb
@@ -1,35 +1,22 @@
 module TimetablesHelper
-  def state_of_cell_l(tag, data, soldier_id)
-    content_tag(tag, "data-action = 'n', date = #{current_day}, soldier_id = #{s.id}") do
-      lesson = Lesson.where(date: data, soldier_id: soldier_id).first
-      patrol = Patrol.where(patrol_start: data, soldier_id: soldier_id).first
-
-      return 'n' unless patrol.nil? #link_to 'н',controller: 'schedules', action: 'new', date: data, soldier_id: soldier_id  unless patrol.nil?
-      return lesson.hours unless lesson.nil? #link_to lesson.hours, controller: 'schedules', action: 'new', date: data, soldier_id: soldier_id  unless lesson.nil?
-      'e' #link_to 'e', controller: 'schedules', action: 'new', date: data, soldier_id: soldier_id
-    end
-  end
-
-
   def state_of_cell(tag, date, soldier_id)
     lesson = Lesson.where(date: date, soldier_id: soldier_id).first
     patrol = Patrol.where(patrol_start: date, soldier_id: soldier_id).first
 
-    action = (patrol.nil? && lesson.nil?) ? 'n' : 'e'  # n - new; e - edit
+    action = (patrol.nil? && lesson.nil?) ? 'n' : 'e' # n - new; e - edit
 
-    if action == 'n'
+    if date.saturday? || date.sunday?
       color = '#faf0e6'
-    elsif date.saturday? || date.sunday?
+    elsif action == 'e'
       color = '#f5f5dc'
     else
       color = '#ffffff'
     end
 
-    str = "data-action = #{action}, date = #{date}, soldier_id = #{soldier_id} style='background-color: #{color}'"
-
-    content_tag(tag, str) do
-      lesson.hours unless lesson.nil?
-      'n' unless patrol.nil?
+    content_tag(tag, style: "background-color: #{color}", data: {action: action, date: date.to_s, soldier_id: soldier_id}) do
+      ch = 'н' unless patrol.nil?
+      ch = lesson.hours.to_s unless lesson.nil?
+      ch
     end
   end
 end
