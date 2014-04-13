ef3349fa61e475fd5bde9d2160757e789ffc4c9c
ars <arsemchik@gmail.com>
2014-04-13 22:36:58 +0300

    added reset password (magic with devise gem)

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 MilitaryOrder::Application.routes.draw do
-  resources :timetables, except: [:show]  do
+  resources :timetables, except: [:show] do
     post :ajax_load_form, on: :collection
   end
   resources :soldiers, except: [:show]
   resources :schedules, except: [:show]
   resources :settings, except: [:show]
 
-  devise_for :users  # temp
+  devise_for :users, skip: [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users/:id' => 'devise/registrations#update', :as => 'user_registration' # temp
+  end
 
   devise_scope :user do
-    get 'login', to: 'devise/sessions#new'
-    post 'login', to: 'devise/sessions#create'
-    delete 'logout', to: 'devise/sessions#destroy'
     root to: 'devise/sessions#new'
   end
 
