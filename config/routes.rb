207d5d45c1b3725adf65661f8392e98fc507a6f8
ars <arsemchik@gmail.com>
2014-04-13 22:39:09 +0300

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ MilitaryOrder::Application.routes.draw do
   resources :timetables, except: [:show] do
     post :ajax_load_form, on: :collection
   end
+
   resources :soldiers, except: [:show]
   resources :schedules, except: [:show]
   resources :settings, except: [:show]
@@ -9,11 +10,7 @@ MilitaryOrder::Application.routes.draw do
   devise_for :users, skip: [:registrations]
   as :user do
     get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    put 'users/:id' => 'devise/registrations#update', :as => 'user_registration' # temp
-  end
-
-  devise_scope :user do
+    put 'users/:id' => 'devise/registrations#update', :as => 'user_registration'
     root to: 'devise/sessions#new'
   end
-
 end
