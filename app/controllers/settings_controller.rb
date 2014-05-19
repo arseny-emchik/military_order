75a59590b419199b10c14c136242aa290b25afea
ars <arsemchik@gmail.com>
2014-05-19 20:25:55 +0300

    added authorize in the settings controller

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,13 +2,19 @@ class SettingsController < ApplicationController
 
   def index
     @users = User.all
+
+    authorize! :index, @users
   end
 
   def edit
+    authorize! :edit, User
+
     @user = User.find(params[:id])
   end
 
   def update
+    authorize! :update, User
+
     @user = User.find(params[:id])
     @user.attributes = user_params
     if @user.save
@@ -19,6 +25,8 @@ class SettingsController < ApplicationController
   end
 
   def destroy
+    authorize! :destroy, User
+
     @user = User.find(params[:id])
     @user.destroy
     redirect_to settings_path
